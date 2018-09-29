f6ab8ea344753fc31802671ded273f3accafb354
shushantkumar <shushantkmr2@gmail.com>
2018-09-29 20:37:55 +0530

    added matlab code

---
+++ sieveof.m
@@ -0,0 +1,18 @@
+clear;
+n = 10000000;
+array = 2:n;
+ones = [];
+for i = 1:length(array)
+    if ~ array(i) == 0 
+        for j = i+1: length(array) 
+            if rem(array(j), array(i)) == 0 
+            array(j) = 0; 
+            end 
+        end 
+    end 
+        if ~ array(i) == 0 
+            ones(length(ones) + 1) = array(i); 
+        end
+end
+
+array
